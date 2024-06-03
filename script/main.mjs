8fd440ca8c9e9921276982ec76eff0a559f28f33
bbcy777 <bbcy777@gmail.com>
2024-06-03 12:04:42 -0700

    cleared radiobtn

--- script/main.mjs
+++ script/main.mjs
@@ -1,6 +1,7 @@
 // import {getNewPhoto, catImages, currentImageIndex, displayImage} from "./data.mjs";
 
 const yesBtn = document.getElementById(`yes`);
+const noBtn = document.getElementById(`no`);
 const imgDiv = document.getElementById(`image`)
 const preBtn = document.getElementById(`preBtn`);
 const nextBtn = document.getElementById("nextBtn");
@@ -8,6 +9,10 @@ const catImages = [];
 let currentImageIndex = -1;
 
 yesBtn.addEventListener(`change`, showPhoto);
+noBtn.addEventListener(`change`, () => {
+    imgDiv.style.display = `none`;
+    clearRadio();
+})
 
 function showPhoto() {
     if(this.checked) {
@@ -21,6 +26,7 @@ function showPhoto() {
             imgDiv.style.display = `none`;
         }
     }
+    clearRadio();
 }
 
 preBtn.addEventListener(`click`, ()=> {
@@ -61,4 +67,9 @@ function displayImage() {
     img.src = catImages[currentImageIndex];
 }
 
-
+function clearRadio() {
+    const radioBtns = document.querySelectorAll(`input`);
+    radioBtns.forEach((radio) => {
+        radio.checked = false;
+    })
+}
